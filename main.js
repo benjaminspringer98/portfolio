7c872b9ba806d1f16acbc83c42496d66297047ed
b.springer <b.springer@stud.uni-goettingen.de>
2022-01-15 00:36:32 +0100

    added basic api to output temperature for given city

--- main.js
+++ main.js
@@ -2,6 +2,43 @@ import * as THREE from 'https://cdn.skypack.dev/three@0.136.0';
 
 import { OrbitControls } from 'https://cdn.skypack.dev/three@0.136.0/examples/jsm/controls/OrbitControls.js'
 
+
+const button = document.querySelector('button');
+const cityNamePara = document.querySelector('#cityName');
+const temperaturePara = document.querySelector('#temperature');
+
+button.addEventListener('click', (e) => {
+  e.preventDefault();
+  let city = document.getElementById('city').value;
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=51efd126ca2090735450e28d2d171946`;
+  Weather.getWeatherInformation(url);
+})
+
+const Weather = (() => {
+  async function getWeatherInformation(url) {
+    try {
+      const response = await fetch(url, {mode: 'cors'});
+      const weatherData = await response.json();
+      console.log(weatherData);
+      DisplayController.addTextContent(weatherData.name, cityNamePara)
+      DisplayController.addTextContent(weatherData.main.temp, temperaturePara)
+    } catch(error) {
+      console.error(error);
+      alert("Please provide a valid city");
+    }
+  }
+
+return {getWeatherInformation}
+})();
+
+const DisplayController = (() => {
+  function addTextContent(content, domElement) {
+    domElement.textContent = content;
+  }
+
+return {addTextContent}
+})();
+
 //initialisation
 const scene = new THREE.Scene();
 
@@ -26,23 +63,18 @@ scene.add(torus);
 //add lighting
 const pointLight = new THREE.PointLight(0xffffff);
 pointLight.position.set(0,0,0);
-
-
 const ambientLight = new THREE.AmbientLight(0xffffff);
-scene.add(pointLight);
+scene.add(pointLight, ambientLight);
 
 //add helpers
 const lightHelper = new THREE.PointLightHelper(pointLight);
 scene.add(lightHelper);
 const gridHelper = new THREE.GridHelper(200, 50);
-//scene.add(gridHelper);
+scene.add(gridHelper);
 
 //add controls
 const controls = new OrbitControls(camera, renderer.domElement);
 
-//add background
-const spaceTexture = new THREE.TextureLoader().load('space.jpg');
-scene.background = spaceTexture;
 
 function animate() {
   requestAnimationFrame(animate);
